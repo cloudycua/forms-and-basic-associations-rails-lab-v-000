6c3eb6f4d2435fc316d155f295e74c43816f9dad
Christine Cua <cloudycua@gmail.com>
2019-05-06 12:44:48 +0000

    Automatically backed up by Learn

--- db/migrate/20190506124253_add_artist_to_songs.rb
+++ db/migrate/20190506124253_add_artist_to_songs.rb
@@ -1,5 +1,5 @@
 class AddArtistToSongs < ActiveRecord::Migration[5.0]
   def change
-    add_reference :songs, :index, foreign_key: true
+    add_reference :songs, :artist, index: true, foreign_key: true
   end
 end
